cfa9309332bb2f7beaa3b0bc831993c342383c92
kino <kinogtag@outlook.com>
2025-03-16 19:28:46 +0000

    Plugin.cs
    Removed BepInDependency as the mod doesn't need Utilla.

--- Plugin.cs
+++ Plugin.cs
@@ -4,7 +4,6 @@ using ComputerModExample;
 
 namespace CustomGamemode
 {
-    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
